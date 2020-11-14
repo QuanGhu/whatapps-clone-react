197483348e02714781d7cab629e61f5e27761afd
Ari Putra <ariteknologi@gmail.com>
2020-11-15 06:07:45 +0700

    add layout chat

---
+++ src/components/Chat.js
@@ -0,0 +1,67 @@
+import React, { useState } from 'react'
+import '../assets/Chat.css'
+import { Avatar, IconButton } from '@material-ui/core'
+import MoreVertIcon from '@material-ui/icons/MoreVert'
+import SearchOutlinedIcon from '@material-ui/icons/SearchOutlined'
+import InsertEmoticonIcon from '@material-ui/icons/InsertEmoticon';
+import MicIcon from '@material-ui/icons/Mic';
+
+function Chat() {
+
+    const [input, setInput] = useState("")
+    const sendMessage = (e) => {
+        e.preventDefault()
+        console.log(`Your input is >> ${input}`)
+    }
+
+    return (
+        <div className="app_chat">
+            <div className="app_chat_header">
+                <Avatar />
+                <div className="app_chat_header_info">
+                    <h3>Room Name</h3>
+                    <p>Last Seen</p>
+                </div>
+                <div className="app_chat_header_right">
+                    <IconButton>
+                        <SearchOutlinedIcon />
+                    </IconButton>
+                    <IconButton>
+                        <MoreVertIcon />
+                    </IconButton>
+                </div>
+            </div>
+            <div className="app_chat_body">
+                <p className="app_chat_message">
+                    <span className="app_chat_name">
+                        Chat Name
+                    </span>
+                    Hi Guys!
+                    <span className="app_chat_time">
+                        12:21 pm
+                    </span>
+                </p>
+
+                <p className={`app_chat_message ${true && 'app_chat_receiver' }`}>
+                    <span className="app_chat_name">
+                        Chat Name
+                    </span>
+                    Hi Guys!
+                    <span className="app_chat_time">
+                        12:21 pm
+                    </span>
+                </p>
+            </div>
+            <div className="app_chat_footer">
+                <InsertEmoticonIcon />
+                <form>
+                    <input type="text" placeholder="Type a message..." value={input} onChange={(e) => setInput(e.target.value)}></input>
+                    <button type="submit" onClick={sendMessage}>send message</button>
+                </form>
+                <MicIcon />
+            </div>
+        </div>
+    )
+}
+
+export default Chat
